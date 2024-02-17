beaed5238f52453f44922f030b8601bdd6c4ed5b
Pep <jjimen2@xtec.cat>
2024-02-17 17:50:13 +0100

    Logs

--- my_app/src/index.jsx
+++ my_app/src/index.jsx
@@ -2,6 +2,16 @@ if (process.env.APP_ENV) {
 	console.log("Environment: " + process.env.APP_ENV)
 }
 
+if (process.env.APP_DEBUG) {
+	console.log("Debug enabled")
+	// Must be the first import
+	import("preact/debug").then((module) => {
+		console.log("'preact/debug' module loaded")	
+	}).catch((error) => {
+		console.error('Error loading module', error)
+	})
+}
+
 import { render } from 'preact';
 import { LocationProvider, Router, Route } from 'preact-iso';
 
@@ -9,8 +19,10 @@ import { Header } from './components/Header.jsx';
 import { Home } from './pages/Home/index.jsx';
 import { NotFound } from './pages/_404.jsx';
 import './style.css';
+import Logger from './library/Logger.js'
 
 export function App() {
+	Logger.debug('App started (this message ONLY appears when debug is enabled)')
 	return (
 		<LocationProvider>
 			<Header />
